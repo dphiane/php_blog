a355ff03ecb772a0b76bfb1f32c0c59dd7dc759c
fz <phiane.d@gmail.com>
2022-12-16 15:38:43 +0100

    CRUD with json

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header>
+    <a href="/" class="logo">Blog</a>
+    <ul class="header-menu">
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/form-article.php' ? 'active' : '' ?>>
+            <a href="/form-article.php">Écrire un article</a>
+        </li>
+    </ul>
+</header>
